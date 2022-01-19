086cc9e04a76d3b1ebe07d81ff515f50c33f1579
kuldeepsagar <kuldeep.sagar@gmail.com>
2022-01-19 16:10:30 +0530

    adding provider block configuration file

---
+++ infrastructure/main/provider.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  #version = "~> 2.0"
+  #access_key = var.access_key 
+  #secret_key = var.secret_key 
+  region     = var.region
+  profile = "default"
+  #profile = "ASBUSER"
+}
